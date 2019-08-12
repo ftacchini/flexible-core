75b3d86191208b843a059a2eac95ceae08c7bde1
Federico <tacchinifederico@gmail.com>
2019-08-12 22:11:54 +0200

    Adding framework interfaces to index

--- src/framework/index.ts
+++ src/framework/index.ts
@@ -1,2 +1,7 @@
 export * from "./flexible-framework";
 export * from "./flexible-pipeline-document";
+export * from "./flexible-activation-context";
+export * from "./flexible-extractor-recipe";
+export * from "./flexible-filter-recipe";
+export * from "./flexible-framework-module";
+export * from "./flexible-middleware-document";

34e518ebf07043a8905cbe42c7ccd96a381931db
Federico Tacchini <tacchf@Federicos-MacBook-Pro.local>
2026-04-03 09:25:30 -0400

    Update extractors index exports

--- src/built-ins/extractors/index.ts
+++ src/built-ins/extractors/index.ts
@@ -4,4 +4,5 @@ export { FullEvent } from "./full-event";
 export { ContextBinnacle } from "./context-binnacle";
 export { PreviousError } from "./previous-error";
 export { PreviousResponse } from "./previous-response";
+
 export { UndefinedValue } from "./undefined-value";
